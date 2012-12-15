155c44a6ce0d759fe6ef9de89630f1a52a2735b7
Hugo Duncan <hugo@hugoduncan.org>
2012-12-14 22:29:43 -0500

    Add release script and profile

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,14 @@
-{:dev
+{:release
+ {:plugins [[lein-set-version "0.2.1"]]
+  :set-version
+  {:updates [{:path "README.md"
+              :no-snapshot true
+              :search-regex
+              #"com.palletops/hadoop-config \"\d+\.\d+\.\d+\""}]}}
+ :dev
  {:dependencies
   [[org.cloudhoist/pallet "0.8.0-SNAPSHOT" :classifier "tests"]
    [ch.qos.logback/logback-classic "1.0.0"]]
-  :repositories
-  {"sonatype" "https://oss.sonatype.org/content/repositories/releases/"}
   :test-selectors {:default (complement :live-test)
                    :live-test :live-test
                    :all (constantly true)}}}
--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "Hadoop configuration library"
   :url "https://github.com/palletops/hadoop-config"
   :license {:name "All rights reserved"}
-  :dependencies [[palletops/locos "0.1.0-SNAPSHOT"]
-                 [org.cloudhoist/pallet "0.8.0-SNAPSHOT"]
-                 [org.clojure/clojure "1.4.0"]])
+  :dependencies [[palletops/locos "0.1.0"]
+                 [org.cloudhoist/pallet "0.8.0-alpha.6"]
+                 [org.clojure/clojure "1.4.0"]]
+  :repositories
+  {"sonatype" {:url "https://oss.sonatype.org/content/repositories/releases/"}})
