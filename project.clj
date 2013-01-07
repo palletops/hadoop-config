15ce72837866f7c12d1cc4f2e1bc45962708e4f4
Hugo Duncan <hugo@hugoduncan.org>
2013-01-07 12:32:55 -0500

    Update to pallet 0.8.0-alpha.7

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/palletops/hadoop-config"
   :license {:name "All rights reserved"}
   :dependencies [[palletops/locos "0.1.0"]
-                 [org.cloudhoist/pallet "0.8.0-alpha.6"]
+                 [org.cloudhoist/pallet "0.8.0-alpha.7"]
                  [org.clojure/clojure "1.4.0"]]
   :repositories
   {"sonatype" {:url "https://oss.sonatype.org/content/repositories/releases/"}}
