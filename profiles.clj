965a6741bd81a140e4283f4d3b124328dc6c77b2
Antoni Batchelli <tbatchelli@acm.org>
2014-04-18 22:08:09 -0700

    remove release code in prep for lein-pallet-release.

--- profiles.clj
+++ profiles.clj
@@ -1,11 +1,4 @@
-{:release
- {:plugins [[lein-set-version "0.2.1"]]
-  :set-version
-  {:updates [{:path "README.md"
-              :no-snapshot true
-              :search-regex
-              #"com.palletops/hadoop-config \"\d+\.\d+\.\d+\""}]}}
- :dev
+{:dev
  {:dependencies
-  [[com.palletops/pallet "0.8.0-beta.8" :classifier "tests"]
+  [[com.palletops/pallet "0.8.0-RC.9" :classifier "tests"]
    [ch.qos.logback/logback-classic "1.0.0"]]}}
