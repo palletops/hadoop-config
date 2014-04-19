ce4da45e8ed6b6ae060ed116c2cef82c99e3dca5
Antoni Batchelli <tbatchelli@acm.org>
2014-04-18 21:58:07 -0700

    Change license to EPL (from All Rights Reserved).

--- project.clj
+++ project.clj
@@ -1,12 +1,9 @@
 (defproject com.palletops/hadoop-config "0.1.1-SNAPSHOT"
   :description "Hadoop configuration library"
   :url "https://github.com/palletops/hadoop-config"
-  :license {:name "All rights reserved"}
-  :dependencies [[com.palletops/locos "0.1.1-SNAPSHOT"]
-                 [com.palletops/pallet "0.8.0-RC.4"]
-                 [org.clojure/clojure "1.4.0"]]
-  :repositories
-  {"sonatype" {:url "https://oss.sonatype.org/content/repositories/releases/"}}
-  :test-selectors {:default (complement :live-test)
-                   :live-test :live-test
-                   :all (constantly true)})
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:url "git@github.com:palletops/hadoop-config.git"}
+  :dependencies [[com.palletops/locos "0.1.1"]
+                 [com.palletops/pallet "0.8.0-RC.9"]
+                 [org.clojure/clojure "1.4.0"]])
