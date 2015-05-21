09025f5e3bd1069347190a400ef782d748830b6f
Hugo Duncan <hugo@hugoduncan.org>
2015-05-21 14:09:22 -0400

    Update dependency versions

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:url "git@github.com:palletops/hadoop-config.git"}
-  :dependencies [[com.palletops/locos "0.1.1"]
-                 [com.palletops/pallet "0.8.0-RC.9"]
-                 [org.clojure/clojure "1.4.0"]])
+  :dependencies [[com.palletops/locos "0.1.2-SNAPSHOT"]
+                 [com.palletops/pallet "0.8.0-RC.10"]
+                 [org.clojure/clojure "1.6.0"]])
